f2711e9826c52bb3a36dfd2edc8e57b5cc01ed53
Haze-I <selamsileshi57@gmail.com>
2022-03-14 08:41:48 -0700

    completed task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point prints the statement in quotes
+ *   *
+ *    *  Return:  a zero if exit is successful, non-zero otherwise
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
