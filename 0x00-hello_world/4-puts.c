e3ea042934c16ffe2217bc05f2b3af387bd4333f
Haze-I <selamsileshi57@gmail.com>
2022-03-14 08:49:30 -0700

    completed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/**
- *  * main - Entry point prints the statement in quotes
- *   *
- *    *  Return:  a zero if exit is successful, non-zero otherwise
- *     */
+/* * 
+ * main - Entry point prints the statement in quotes
+ * 
+ * Return:  a zero if exit is successful, non-zero otherwise
+ * */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
