a81d5939abf9299f98816ff55e04756ae47bbd41
Haze-I <selamsileshi57@gmail.com>
2022-03-14 09:11:37 -0700

    completed task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point prints the statement in the quotes
+ *
+ *Return: return a zero if successful non-zero otherwise
+ */
+int main(void){
+	printf('with proper grammar, but the outcome is a piece of art,\n');
+	return(void);
+}
+
