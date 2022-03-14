a46555e41a7d2870d1d47d33dce06698f2ce820c
Haze-I <selamsileshi57@gmail.com>
2022-03-14 08:55:43 -0700

    Fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/* * 
+/** 
  * main - Entry point prints the statement in quotes
  * 
  * Return:  a zero if exit is successful, non-zero otherwise
- * */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
