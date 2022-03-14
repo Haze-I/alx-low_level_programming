a2a7677dcdf490fd8b99447bb21e5bdc361b7e39
Haze-I <selamsileshi57@gmail.com>
2022-03-14 13:56:00 -0700

    completed task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - prints the alphabet in lowecase
+Return: 0 (Always Success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
